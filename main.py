be9178f1051d59cbfae69fe85987328a69014c0f
dengzhihui <dengzhihui.com@gmail.com>
2023-08-06 13:30:05 +0800

    init

---
+++ main.py
@@ -0,0 +1,90 @@
+import requests
+import time
+import datetime
+import os
+from dotenv import load_dotenv
+
+
+def send_pushover_messages(token, group, device, message):
+    import requests
+
+    # Pushover API url
+    url = "https://api.pushover.net/1/messages.json"
+
+    # Send the message to each user
+    # Send the message to the group
+    data = {
+        "token": token,
+        "user": group,
+        "message": message,
+        "device": device,
+    }
+    response = requests.post(url, data=data)
+    print(f"Sent message to {group}, status: {response.status_code}")
+
+
+# pushover_token = "azt8e3gnx1zdhicdg7kuz9y64mbznn"
+# pushover_group = "gbcw19yoe42yikdsxitetfcwc82557"
+# pushover_device = "yz_iphonexsmax"
+
+# Load the .env file
+load_dotenv()
+
+pushover_token = os.getenv("PUSHOVER_TOKEN", "azt8e3gnx1zdhicdg7kuz9y64mbznn")
+pushover_group = os.getenv("PUSHOVER_GROUP", "gbcw19yoe42yikdsxitetfcwc82557")
+pushover_device = os.getenv("PUSHOVER_DEVICE_NAME", "yz_iphonexsmax")
+
+# Define constants
+# Interval to check the car status in seconds
+check_interval = float(os.getenv("CHECK_INTERVAL", 60))
+URL = "http://10.168.1.115:8081/api/v1/cars/1/status"  # URL to fetch car status
+
+REMIND_TIMES = [8, 12, 20]  # Times to remind every day
+# Convert environment variables to appropriate data types
+speed_limit = float(os.getenv("SPEED_LIMIT", 0))
+battery_level_limit = float(os.getenv("BATTERY_LEVEL_LIMIT", 35))
+geofence = os.getenv("GEOFENCE", "家")
+REMIND_TIMES = list(map(int, os.getenv("REMIND_TIMES", "8,12,20").split(",")))
+
+
+last_remind = None  # The time of the last remind
+
+# Define a list to hold the remind times
+remind_done = []
+
+while True:
+    # Fetch car status
+    response = requests.get(URL)
+    data = response.json()
+
+    # Check the conditions
+    driving_details = data["data"]["status"]["driving_details"]
+    battery_details = data["data"]["status"]["battery_details"]
+    car_geodata = data["data"]["status"]["car_geodata"]
+
+    if (
+        driving_details["speed"] <= speed_limit  # 0
+        and battery_details["battery_level"] <= battery_level_limit  # 35
+        and car_geodata["geofence"] == geofence  # "家"
+    ):
+        # Check if it's time to remind
+        now = datetime.datetime.now()
+        hour = now.hour
+        today = now.date()
+        timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+        battery_level = battery_details["battery_level"]
+
+        print(f"{timestamp}: 条件满足，检查是否需要发出提醒...", end="")
+
+        if (today not in remind_done) or (
+            hour in REMIND_TIMES and today not in remind_done
+        ):
+            msg = f"充电提醒，当前电量：{battery_level}%"
+            print(msg)
+            send_pushover_messages(pushover_token, pushover_group, pushover_device, msg)
+            remind_done.append(today)
+        else:
+            print("不需要提醒")
+
+    # Sleep until the next check
+    time.sleep(check_interval)
