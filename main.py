7b5ad2355712bcf98ca4a1c94475a18f44aee45c
frankdzh <dengzhihui.com@gmail.com>
2023-08-08 09:43:49 +0800

    只在跨越临界值时通知一次 优化提示

--- main.py
+++ main.py
@@ -60,9 +60,10 @@ logging.basicConfig(level=logging.INFO)
 last_remind = None  # The time of the last remind
 
 # Define a list to hold the remind times
-remind_done = []
+battery_alarm_triggered = []  # 只在跨越临界值时通知一次
 remind_time_done = []
 last_date = datetime.datetime.now().date() - datetime.timedelta(days=1)
+last_battery = 0
 
 logging.info(f"启动检查电量 <= {battery_level_limit}%...")
 while True:
@@ -74,46 +75,52 @@ while True:
     # Check the conditions
     driving_details = data["data"]["status"]["driving_details"]
     battery_details = data["data"]["status"]["battery_details"]
-    car_geodata = data["data"]["status"]["car_geodata"]    
+    car_geodata = data["data"]["status"]["car_geodata"]
+    current_battery = battery_details["battery_level"]
     
     timestamp = get_timestamp()
-    logging.info(f"{timestamp}: 检查-> 当前电量:{battery_details['battery_level']}% <= 报警电量:{battery_level_limit}%, 当前速度:{driving_details['speed']} <= 报警速度:{speed_limit}, 当前位置:{car_geodata['geofence']} == 报警位置:{geofence}...")
+    logging.info(
+        f"{timestamp}: 检查-> 当前电量:{current_battery}% <= 报警电量:{battery_level_limit}%, 当前速度:{driving_details['speed']} <= 报警速度:{speed_limit}, 当前位置:{car_geodata['geofence']} == 报警位置:{geofence}...")
 
     if (
         driving_details["speed"] <= speed_limit  # 0
-        and battery_details["battery_level"] <= battery_level_limit  # 35
+        and current_battery <= battery_level_limit  # 35
         and car_geodata["geofence"] == geofence  # "家"
     ):
         # Check if it's time to remind
         now = datetime.datetime.now()
         hour = now.hour
         today = now.date()
-        timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
-        battery_level = battery_details["battery_level"]
+        timestamp = now.strftime("%Y-%m-%d %H:%M:%S")        
 
         if (last_date != today):
-            remind_done = []
+            battery_alarm_triggered = []
             remind_time_done = []
             last_date = today
 
         print(f"{timestamp}: 条件满足，检查是否需要发出提醒...", end="")
         logging.info(f"{timestamp}: 条件满足，检查是否需要发出提醒...")
+        bTriggerToday = current_battery < last_battery and current_battery == battery_level_limit and today not in battery_alarm_triggered
+        bPeriodic_reminder = hour in REMIND_TIMES and hour not in remind_time_done
 
-        if (today not in remind_done) or (
-            hour in REMIND_TIMES and hour not in remind_time_done
-        ):
-            msg = f"充电提醒，当前电量：{battery_level}%"
+        if (bTriggerToday) or (
+            bPeriodic_reminder
+        ):            
+            if (bTriggerToday):
+                battery_alarm_triggered.append(today)
+                msg = f"充电提醒，当前电量：{current_battery}% 首次 < {battery_level_limit}"
+            if (bPeriodic_reminder):
+                remind_time_done.append(hour)
+                msg = f"定期充电提醒，当前电量：{current_battery}% < {battery_level_limit}"
+            
             #print(msg)
             logging.info(f"{timestamp}: {msg}")
-            send_pushover_messages(pushover_token, pushover_group, pushover_device, msg)
-            
-            if (today not in remind_done):
-                remind_done.append(today)
-            if (hour in REMIND_TIMES):
-                remind_time_done.append(hour)
+            send_pushover_messages(
+                pushover_token, pushover_group, pushover_device, msg)
         else:
             #print("不需要提醒")
             logging.info("不需要提醒")
 
+    last_battery = current_battery
     # Sleep until the next check
     time.sleep(check_interval)
