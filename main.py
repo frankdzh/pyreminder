bb72520b958a488fe5f6219cdc7864963055c712
frankdzh <dengzhihui.com@gmail.com>
2023-08-08 13:11:11 +0800

    增加单元测试代码 优化主流程的代码结构 增加环境变量设置项

--- main.py
+++ main.py
@@ -5,122 +5,112 @@ import os
 from dotenv import load_dotenv
 import logging
 
+# Load environment variables from .env file
+load_dotenv()
+logging.basicConfig(level=logging.INFO)
+
+pushover_token = os.getenv("PUSHOVER_TOKEN")
+pushover_user = os.getenv("PUSHOVER_GROUP")
+pushover_device = os.getenv("PUSHOVER_DEVICE_NAME")
+
+URL = os.getenv("CAR_STATUS_URL")
+speed_limit = int(os.getenv("SPEED_LIMIT"))
+battery_level_limit = int(os.getenv("BATTERY_LEVEL_LIMIT"))
+REMIND_TIMES = [int(x) for x in os.getenv("REMIND_TIMES").split(",")]
+car_geofence_limit = os.getenv("CAR_GEOFENCE", "家")
 
 pushover_enabled = os.getenv("PUSHOVER_ENABLED", "false")
 pushover_enabled = pushover_enabled.lower() == "true"
+
+check_interval = float(os.getenv("CHECK_INTERVAL", 60))
+battery_alarm_triggered = False  # 只在跨越临界值时通知一次
+last_battery = 0
+last_date = datetime.datetime.now().date() - datetime.timedelta(days=1)
 
 def get_timestamp():
     now = datetime.datetime.now()
     timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
     return timestamp
 
-def send_pushover_messages(token, group, device, message):
-    import requests
-
-    # Pushover API url
-    url = "https://api.pushover.net/1/messages.json"
-
-    # Send the message to each user
-    # Send the message to the group
-    data = {
-        "token": token,
-        "user": group,
-        "message": message,
-        "device": device,
-    }
-    timestamp = get_timestamp()
+def send_pushover_messages(token, user, device, message):
     if (pushover_enabled):
+        # Pushover API url
+        url = "https://api.pushover.net/1/messages.json"
+
+        # Send the message to each user
+        # Send the message to the group
+        data = {
+            "token": token,
+            "user": user,
+            "message": message,
+            "device": device,
+        }
         response = requests.post(url, data=data)
         #print(f"Sent message to {group}, status: {response.status_code}")
-        logging.info(f"{timestamp}:Sent message '{message}' to {group}, status: {response.status_code}")
+        logging.info(f"{get_timestamp()}:发送消息 '{message}' to {user}, status: {response.status_code}")
     else:
-        logging.info(f"{timestamp}:Not Sent message '{message}' to {group}")
+        logging.info(f"{get_timestamp()}:禁用了发送消息 '{message}' to {user}")
 
-# Load the .env file
-load_dotenv()
-
-pushover_token = os.getenv("PUSHOVER_TOKEN", "azt8e3gnx1zdhicdg7kuz9y64mbznn")
-pushover_group = os.getenv("PUSHOVER_GROUP", "gbcw19yoe42yikdsxitetfcwc82557")
-pushover_device = os.getenv("PUSHOVER_DEVICE_NAME", "yz_iphonexsmax")
-
-# Define constants
-# Interval to check the car status in seconds
-check_interval = float(os.getenv("CHECK_INTERVAL", 60))
-URL = "http://10.168.1.115:8081/api/v1/cars/1/status"  # URL to fetch car status
-
-REMIND_TIMES = [8, 12, 20]  # Times to remind every day
-# Convert environment variables to appropriate data types
-speed_limit = float(os.getenv("SPEED_LIMIT", 0))
-battery_level_limit = float(os.getenv("BATTERY_LEVEL_LIMIT", 35))
-geofence = os.getenv("GEOFENCE", "家")
-REMIND_TIMES = list(map(int, os.getenv("REMIND_TIMES", "8,12,20").split(",")))
-
-logging.basicConfig(level=logging.INFO)
-
-last_remind = None  # The time of the last remind
-
-# Define a list to hold the remind times
-battery_alarm_triggered = []  # 只在跨越临界值时通知一次
-remind_time_done = []
-last_date = datetime.datetime.now().date() - datetime.timedelta(days=1)
-last_battery = 0
-
-logging.info(f"启动检查电量 <= {battery_level_limit}%...")
-while True:
-
-    # Fetch car status
-    response = requests.get(URL)
-    data = response.json()
-
-    # Check the conditions
-    driving_details = data["data"]["status"]["driving_details"]
-    battery_details = data["data"]["status"]["battery_details"]
-    car_geodata = data["data"]["status"]["car_geodata"]
-    current_battery = battery_details["battery_level"]
-    
-    timestamp = get_timestamp()
-    logging.info(
-        f"{timestamp}: 检查-> 当前电量:{current_battery}% <= 报警电量:{battery_level_limit}%, 当前速度:{driving_details['speed']} <= 报警速度:{speed_limit}, 当前位置:{car_geodata['geofence']} == 报警位置:{geofence}...")
-
+def check_car_status_and_send_reminders(
+    today, hour, current_battery, current_speed, current_cargeo
+):
     if (
-        driving_details["speed"] <= speed_limit  # 0
+        current_speed <= speed_limit  # 0
         and current_battery <= battery_level_limit  # 35
-        and car_geodata["geofence"] == geofence  # "家"
+        and current_cargeo == car_geofence_limit  # "家"
     ):
-        # Check if it's time to remind
-        now = datetime.datetime.now()
-        hour = now.hour
-        today = now.date()
-        timestamp = now.strftime("%Y-%m-%d %H:%M:%S")        
+
+        global last_date, battery_alarm_triggered, remind_time_done, last_battery
+        #now = datetime.datetime.now()
+        #hour = now.hour
+        #today = now.date()
 
         if (last_date != today):
-            battery_alarm_triggered = []
             remind_time_done = []
             last_date = today
 
-        print(f"{timestamp}: 条件满足，检查是否需要发出提醒...", end="")
-        logging.info(f"{timestamp}: 条件满足，检查是否需要发出提醒...")
-        bTriggerToday = current_battery < last_battery and current_battery == battery_level_limit and today not in battery_alarm_triggered
+        bTriggerOnce = current_battery < last_battery and current_battery == battery_level_limit and battery_alarm_triggered == False
         bPeriodic_reminder = hour in REMIND_TIMES and hour not in remind_time_done
+        
+        if bTriggerOnce:
+            msg = f"跨越边界提醒，当前电量：{current_battery}% < {battery_level_limit}"
+            logging.info(f"{get_timestamp()}: {msg}")
+            send_pushover_messages(pushover_token, pushover_user, pushover_device, msg)
+            battery_alarm_triggered = True
+            return "跨越边界提醒已发送"
 
-        if (bTriggerToday) or (
-            bPeriodic_reminder
-        ):            
-            if (bTriggerToday):
-                battery_alarm_triggered.append(today)
-                msg = f"充电提醒，当前电量：{current_battery}% 首次 < {battery_level_limit}"
-            if (bPeriodic_reminder):
-                remind_time_done.append(hour)
-                msg = f"定期充电提醒，当前电量：{current_battery}% < {battery_level_limit}"
-            
-            #print(msg)
-            logging.info(f"{timestamp}: {msg}")
-            send_pushover_messages(
-                pushover_token, pushover_group, pushover_device, msg)
-        else:
-            #print("不需要提醒")
-            logging.info("不需要提醒")
+        if bPeriodic_reminder:
+            msg = f"定期充电提醒，当前电量：{current_battery}%"
+            logging.info(f"{get_timestamp()}: {msg}")
+            send_pushover_messages(pushover_token, pushover_user, pushover_device, msg)
+            remind_time_done.append(hour)
+            return "定期充电提醒已发送"
+    
+        if (current_battery > battery_level_limit):
+            battery_alarm_triggered = False
 
-    last_battery = current_battery
-    # Sleep until the next check
-    time.sleep(check_interval)
+    return "无需提醒"
+
+def main():
+    # Main loop (will be executed in the actual environment)
+    while True:
+        response = requests.get(URL)
+        data = response.json()
+        # Simulate getting current battery level
+        current_battery = data["data"]["status"]["battery_details"]["battery_level"]
+        current_speed = data["data"]["status"]["driving_details"]["speed"]
+        current_cargeo = data["data"]["status"]["car_geodata"]['geofence']
+
+        logging.info(
+            f"{get_timestamp()}: 检查-> 当前电量:{current_battery}% <= 报警电量:{battery_level_limit}%, 当前速度:{current_speed} <= 报警速度:{speed_limit}, 当前位置:{current_cargeo} == 报警位置:{car_geofence_limit}...")
+        
+        now = datetime.datetime.now()
+        hour = now.hour
+        today = now.date()
+        check_car_status_and_send_reminders(today, hour, current_battery, current_speed, current_cargeo)
+
+        last_battery = current_battery
+        time.sleep(check_interval)
+
+
+main()
---
+++ test.py
@@ -0,0 +1,136 @@
+import datetime
+import unittest
+import responses
+import os
+import main  # Assuming the original code is in a file named main.py
+import logging
+
+
+class IntegrationTest(unittest.TestCase):
+
+    def setUp(self):
+        # Setting up environment variables required for the test
+        #os.environ['SPEED_LIMIT'] = '50'
+        #os.environ['BATTERY_LEVEL_LIMIT'] = '30'
+        #os.environ['REMIND_TIMES'] = '10,12'
+        # ... set other environment variables as needed
+        os.environ['REMIND_TIMES111'] = '10,12'
+
+    @responses.activate
+    def test_battery_boundary_reminder(self):
+        logging.getLogger().setLevel(logging.WARN)
+        # Mocking the API response for the car status
+        #responses.add(responses.GET, main.URL,
+        #              json={"data": {"status": {"battery_details": {"battery_level": 30},
+        #                                        "driving_details": {"speed": 0},
+        #                                        "car_geodata": {'geofence': '家'}}}}, status=200)
+        now = datetime.datetime.now()
+        hour = now.hour
+        today = now.date()
+
+        # 跨越边界========================================================
+        main.last_battery = 36
+        current_battery = 35
+        result = main.check_car_status_and_send_reminders(today, 3, current_battery, 0, "家")
+        self.assertEqual(result, "跨越边界提醒已发送")
+        #已经通知过了
+        main.battery_alarm_triggered = True
+        result = main.check_car_status_and_send_reminders(today, 3, current_battery, 0, "家")
+        self.assertEqual(result, "无需提醒")
+        
+        #已经通知过了, 但是过了一段时间后，电池又达到临界条件
+        main.last_battery = 36
+        main.battery_alarm_triggered = False
+        result = main.check_car_status_and_send_reminders(today, 3, current_battery, 0, "家")
+        main.last_battery = 36
+        current_battery = 35
+        result = main.check_car_status_and_send_reminders(today, 3, current_battery, 0, "家")
+        self.assertEqual(result, "无需提醒")
+
+        #已经通知过了,第二天充电后，电池又达到临界条件
+        main.last_battery = 36        
+        main.battery_alarm_triggered = False
+        current_battery = 40
+        result = main.check_car_status_and_send_reminders(today, 3, current_battery, 0, "家")
+        main.last_battery = 36
+        day = today + datetime.timedelta(days=1)
+        main.last_date = day - datetime.timedelta(days=1)
+        current_battery = main.battery_level_limit
+        result = main.check_car_status_and_send_reminders(day, 3, current_battery, 0, "家")
+        self.assertEqual(result, "跨越边界提醒已发送")
+        #=================================================================
+
+        #电量=============================================================
+        main.last_battery = 35
+        main.battery_alarm_triggered = False
+        result = main.check_car_status_and_send_reminders(today, 3, 35, 0, "家")
+        self.assertEqual(result, "无需提醒")
+        
+        main.last_battery = main.battery_level_limit + 1
+        result = main.check_car_status_and_send_reminders(today, 3, main.battery_level_limit, 0, "家")
+        self.assertEqual(result, "跨越边界提醒已发送")
+        # > 
+        result = main.check_car_status_and_send_reminders(today, 10, main.battery_level_limit + 10, 0, "家")
+        self.assertEqual(result, "无需提醒")
+        
+        #跨天测试
+        main.last_date = today - datetime.timedelta(days=1)
+        result = main.check_car_status_and_send_reminders(today, 0, 30, 0, "家")
+        self.assertEqual(result, "无需提醒")
+
+    @responses.activate
+    def test_scheduled_reminders(self):
+        logging.getLogger().setLevel(logging.WARN)
+        # Mocking the API response for the car status
+        #responses.add(responses.GET, main.URL,
+        #              json={"data": {"status": {"battery_details": {"battery_level": 40},
+        #                                        "driving_details": {"speed": 0},
+        #                                        "car_geodata": {'geofence': '家'}}}}, status=200)
+        now = datetime.datetime.now()
+        today = now.date()
+        main.last_battery = 35        
+        # Mocking the datetime to be at one of the remind times
+        # ...
+        main.battery_alarm_triggered = True
+        result = main.check_car_status_and_send_reminders(today, 6, 35, 0, "家")
+        self.assertEqual(result, "定期充电提醒已发送")
+        result = main.check_car_status_and_send_reminders(today, 12, 35, 0, "家")
+        self.assertEqual(result, "定期充电提醒已发送")
+        result = main.check_car_status_and_send_reminders(today, 18, 35, 0, "家")
+        self.assertEqual(result, "定期充电提醒已发送")
+        result = main.check_car_status_and_send_reminders(today, 22, 35, 0, "家")
+        self.assertEqual(result, "定期充电提醒已发送")
+
+        result = main.check_car_status_and_send_reminders(today, 6, 35, 0, "家")
+        self.assertEqual(result, "无需提醒")
+        result = main.check_car_status_and_send_reminders(today, 12, 35, 0, "家")
+        self.assertEqual(result, "无需提醒")
+        result = main.check_car_status_and_send_reminders(today, 18, 35, 0, "家")
+        self.assertEqual(result, "无需提醒")
+        result = main.check_car_status_and_send_reminders(today, 22, 35, 0, "家")
+        self.assertEqual(result, "无需提醒")
+
+        main.battery_alarm_triggered = False
+        main.remind_time_done = []
+        result = main.check_car_status_and_send_reminders(today, 6, 35, 0, "家")
+        self.assertEqual(result, "定期充电提醒已发送")
+        result = main.check_car_status_and_send_reminders(today, 12, 35, 0, "家")
+        self.assertEqual(result, "定期充电提醒已发送")
+        result = main.check_car_status_and_send_reminders(today, 18, 35, 0, "家")
+        self.assertEqual(result, "定期充电提醒已发送")
+        result = main.check_car_status_and_send_reminders(today, 22, 35, 0, "家")
+        self.assertEqual(result, "定期充电提醒已发送")
+
+        result = main.check_car_status_and_send_reminders(today, 6, 35, 0, "家")
+        self.assertEqual(result, "无需提醒")
+        result = main.check_car_status_and_send_reminders(today, 12, 35, 0, "家")
+        self.assertEqual(result, "无需提醒")
+        result = main.check_car_status_and_send_reminders(today, 18, 35, 0, "家")
+        self.assertEqual(result, "无需提醒")
+        result = main.check_car_status_and_send_reminders(today, 22, 35, 0, "家")
+        self.assertEqual(result, "无需提醒")
+
+
+# Running the tests
+if __name__ == '__main__':
+    unittest.main()
